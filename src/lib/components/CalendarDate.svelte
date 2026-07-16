380959df13bb871c0e8eb735b25edb4e47439a5c
kenny <kenjlee09@gmail.com>
2026-07-16 12:58:15 -0700

    hi

---
+++ src/lib/components/CalendarDate.svelte
@@ -0,0 +1,31 @@
+
+<script>
+    import { translate } from "$lib/translate";
+    function openModal() {
+        // Open the modal here
+    }
+    let {
+        schedule,
+        date,
+    }: {
+        schedule: 
+
+        }
+    } = $props();
+
+
+</script>
+
+<button type="button" class="calendar-day-wrap" onclick = {openModal()}>
+    <div class="calendar-box">
+        <div class="cd-number"> { date.date() } </div>
+        <div class="cd-tag"> { schedule.scheduleType } </div>
+    </div>
+</button>
+
+
+<!-- Steps:
+ 1. set dates on box
+ 2. make it say what type of day it is
+ 3. make it modal on click
+ 4. modal should display the schedule on that specific day -->
